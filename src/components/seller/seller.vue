6fb7b6a404f22b59547edb6d3619823002737232
xiaoq <xiaoq>
2019-04-12 17:38:22 +0800

    1

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -3,7 +3,7 @@
         <div class="seller-content">
             <!--概述-->
             <div class="overview">
-                <h1 class="title">{{seller.name}}</h1> 
+                <h1 class="title">{{seller.name}}</h1>
                 <div class="desc">
                     <star :size="36" :score="seller.score"></star>
                     <span class="text">({{seller.ratingCount}})</span>
@@ -30,6 +30,22 @@
                     </li>
                 </ul>
             </div>
+            <!--灰色分隔条-->
+            <split></split>
+            <!--公告与活动-->
+            <div class="bulletin">
+                <h1 class="title">公告与活动</h1>
+                <div class="content-wrapper">
+                    <p class="content">{{seller.bulletin}}</p>
+                </div>
+                <!--活动列表-->
+                <ul class="supports" v-if="seller.supports">
+                    <li v-for="(i,index) in seller.supports" :key="index" class="supports-item">
+                        <cover-icon :sub="2" :index="i.type"></cover-icon>
+                        <span class="text">{{i.description}}</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
@@ -37,6 +53,7 @@
 <script type="text/ecmascript-6">
 // import BScroll from 'better-scroll';// 加载 better-scroll
 import star from '../star/star';// 星星组件
+import split from '../split/split';// 样式，100%宽度的灰色分割条
 export default{
     props: {
         seller: {
@@ -44,7 +61,8 @@ export default{
         }
     },
     components: {
-        star
+        star,
+        split
     }
 };
 </script>
@@ -67,18 +85,53 @@ export default{
             color: rgb(7,17,27)
             font-size:14px
         .desc
-            padding-bottom :18px
+            padding-bottom :13px
             line-height :18px
             font-size:0
             border-1px(rgba(1,17,27,0.1))
             .star
-                display: ineline-block
-                vertical-align :middle
+                display: inline-block
+                vertical-align :top
                 margin-right:8px
             .text
                 margin-right:12px
                 display: inline-block
-                vertical-align :middle
-                font-szie:10px
+                vertical-align :top
+                font-size:10px
                 color:rgb(77,85,93)
+        .remark
+            display :flex
+            padding-top:18px
+            .block
+                flex:1
+                text-align: center
+                border-right:1px solid rgba(1,17,27,0.1)
+                &:last-child
+                    border:none
+                h2
+                    margin-bottom:4px
+                    line-height :10px
+                    font-size:10px
+                    color:rgb(147,153,159)
+                .content
+                    line-height:24px
+                    font-size:10px
+                    color:rgb(7,17,27)
+                    .stress
+                        font-size:24px
+    .bulletin
+        box-sizizing:border-box
+        padding:18px 18px 0 18px
+        .title
+            margin-bottom :8px
+            line-height :14px
+            color: rgb(1,17,27)
+        .content-wrapper
+            padding:0 12px 16px
+            border-1px()
+            .content
+                line-height :24px
+                font-size:12px
+                color: rgb()
+        .supports
 </style>
